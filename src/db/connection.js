69ee09d04f117f580dc33bb80b9a72c37a9b9344
Muneeb381a <muneebafzal381a@gmail.com>
2025-02-15 09:26:57 +0500

    Add database connection module for PostgreSQL

---
+++ src/db/connection.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+
+import { Client } from 'pg';
+
+dotenv.config({
+    path: "./.env"
+});
+
+let client;
+
+const connectDB = async () => {
+    try {
+        client = new Client({
+            connectionString: process.env.DATABSE_URL,
+            ssl: {
+                rejectUnauthorized: false // this is required for Neon DB database
+            }
+        });
+        client.connect();
+
+        console.log("✅ Connected to PostgreSQL on Neon DB");
+        
+    } catch (error) {
+        console.error(`❌ Unable to connect to PostgreSQL on Neon DB, ${error}`);
+        throw new Error(error);
+    }
+}
+
+export { connectDB, client };
